46b48735eb0d87e88319936b67d2c8cdf0aa9b48
bharathautomation <soft.ittech20@gmail.com>
2025-11-19 09:46:33 +0530

    access & non-access modifers

---
+++ src/non/access/modifiers/AbsClass2.java
@@ -0,0 +1,22 @@
+package non.access.modifiers;
+
+public  class AbsClass2 extends AbstractNonAccessModifier {
+
+	public static void main(String[] args) {
+		
+
+	}
+
+	@Override
+	void login() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	void logout() {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
---
+++ src/non/access/modifiers/NonAccessModifiers.java
@@ -0,0 +1,12 @@
+package non.access.modifiers;
+
+public class NonAccessModifiers {
+	
+	//Access Modifiers: public, private, protected, default ( to control the access)
+	//Non-Access Modifiers: static, abstract, final, synchronized, volatile, transient (to control the behavior)
+
+	public static void main(String[] args) {
+
+	}
+
+}
