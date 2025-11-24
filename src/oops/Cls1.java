6e9fcebdf0943226ccf09b0e468594bfaaac3630
bharathautomation <139088966+bharathtechacademy@users.noreply.github.com>
2025-11-24 09:31:45 +0530

    OOPS

---
+++ src/oops/Cls1.java
@@ -0,0 +1,20 @@
+package oops;
+
+public class Cls1 {
+	
+	private int age = 30;
+	
+	public int getAge() {
+		return age;
+	}
+	
+	public void setAge(int newAge) {
+		age = newAge;
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ src/oops/Cls2.java
@@ -0,0 +1,12 @@
+package oops;
+
+public class Cls2 {
+
+	public static void main(String[] args) {
+		
+		Cls1 obj = new Cls1();
+		System.out.println("Age is: " + obj.getAge());
+
+	}
+
+}
